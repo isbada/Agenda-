f9f186e6997427fd9923b082e752a1ae4a58ba86
fh <is_bada@fuhaodembp.lan>
2017-03-15 21:26:01 +0800

    Initial Commit

---
+++ Agenda/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Agenda
+//
+//  Created by fh on 2017/3/15.
+//  Copyright © 2017年 付豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
